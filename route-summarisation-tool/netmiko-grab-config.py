f530b097c3285a304353a72551c1f94bca22bb90
klimpsch <j.klimpsch@protonmail.com>
2026-09-06 14:47:10 +1000

    Added ssh to routers, checked Netmiko working across all.

--- route-summarisation-tool/netmiko-grab-config.py
+++ route-summarisation-tool/netmiko-grab-config.py
@@ -2,7 +2,25 @@ from netmiko import ConnectHandler
 from netmiko import NetmikoTimeoutException, NetmikoAuthenticationException
 
 
-                              
+ip_list = [
+    '10.0.255.1',
+    '10.1.255.101',
+    '10.1.255.102',
+    '10.1.255.103',
+    '10.0.255.3',
+    '10.3.255.101',
+    '10.3.255.102',
+    '10.3.255.103',
+    '10.3.255.104',
+    '10.0.255.4',
+    '10.4.255.101',
+    '10.4.255.102',
+    '10.4.255.103',
+    '10.2.255.101',
+    '10.2.255.102',
+    '10.2.255.103',
+    '10.2.255.104'
+]
 
 arr = ['192.168.0.190']
 def connect_netmiko(host):
@@ -27,7 +45,7 @@ routing_table = {}
 
 """ base logic for the config grab """
 
-for a in arr:
+for a in ip_list:
     output = connect_netmiko(a)
     for i in output:
         if isinstance(i, dict):
@@ -36,4 +54,5 @@ for a in arr:
             if network and prefix:
                 routing_table[network] = prefix
 
-
+for key, value in routing_table.items():
+    print(key, value)
